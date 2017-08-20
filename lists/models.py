e166c48f9284b2c24b074da8e17c9e31a49642cd
justin <masterja29@gmail.com>
2017-08-20 12:30:12 -0400

    redirect after POST, and show all items in template

--- lists/models.py
+++ lists/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class Item(models.Model):
     text = models.TextField(default='')
+
+    # this is to get rid of the pylint .objects warning
+    objects = models.Manager()
