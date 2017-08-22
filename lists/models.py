8b84d1892473670d173cbe5429019368c489c756
justin <masterja29@gmail.com>
2017-08-21 22:20:27 -0400

    adjusted model so items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+class List(models.Model):
+    pass
+
+    # this is to get rid of the pylint .objects warning
+    objects = models.Manager()
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
 
     # this is to get rid of the pylint .objects warning
     objects = models.Manager()
